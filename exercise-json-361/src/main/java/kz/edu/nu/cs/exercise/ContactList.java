79d2c1b0955ac9cf82cbb6de91c75f0c3436e3b1
Kassym Akhmetbek <kassym.akhmetbek@nu.edu.kz>
2018-08-27 00:56:10 +0600

    Kassym Akhmetbek. Lab3 Sorry for late pull request. Couldn't manage the network while being in the city

---
+++ exercise-json-361/src/main/java/kz/edu/nu/cs/exercise/ContactList.java
@@ -0,0 +1,38 @@
+package kz.edu.nu.cs.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
+
+public class ContactList {
+    private List<MyContact> list;
+    
+    public ContactList() {
+        list = new ArrayList<MyContact>();
+    }
+    
+    public static ContactList MakeContactList(String s) {
+        Gson gson = new Gson();
+
+        return gson.fromJson(s, ContactList.class);
+    }
+    
+    public void addContact(MyContact c) {
+        list.add(c);
+    }
+    
+    public void removeFirstContact() {
+        list.remove(0);
+    }
+    
+    public MyContact getFirstContact() {
+
+        return list.get(0);
+    }
+    
+    public int getSize() {
+        // complete this method
+        // return correct value
+        return list.size();
+    }
+}
